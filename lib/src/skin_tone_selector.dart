bc842018debda2ab1510a2726d96088be0528b61
Definev <daiduong.workmail@gmail.com>
2024-07-09 00:55:57 +0700

    chore: Update flutter_portal dependency to version 1.1.4

--- lib/src/skin_tone_selector.dart
+++ lib/src/skin_tone_selector.dart
@@ -2,6 +2,7 @@ import 'package:design_system/design_system.dart';
 import 'package:emoji_selector/src/skin_dot.dart';
 import 'package:emoji_selector/src/skin_tones.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_portal/flutter_portal.dart';
 import 'package:mix/mix.dart';
 
 class SkinToneSelector extends StatefulWidget {
@@ -18,76 +19,47 @@ class SkinToneSelector extends StatefulWidget {
 
 class _SkinToneState extends State<SkinToneSelector> {
   int _skin = 0;
-  OverlayEntry? _overlayEntry;
   bool _expanded = false;
-  FocusNode focusNode = FocusNode();
-
-  OverlayEntry createOverlay(BuildContext context) {
-    RenderBox renderBox = context.findRenderObject() as RenderBox;
-    var size = renderBox.size;
-    var offset = renderBox.localToGlobal(Offset.zero);
-
-    final width =
-        size.height * SkinTones.tones.length + SkinTones.tones.length + 1;
-    return OverlayEntry(
-      builder: (context) => Positioned(
-        left: offset.dx - width + size.width,
-        top: offset.dy - size.height,
-        height: size.height,
-        width: width,
-        child: FocusableActionDetector(
-          focusNode: focusNode,
-          onShowFocusHighlight: (value) =>
-              value ? null : _overlayEntry?.remove(),
-          child: DSToolbar(
-            direction: Axis.horizontal,
-            children: [
-              for (var skin = 0; skin < SkinTones.tones.length; skin++)
-                DSToolbarItem(
-                  style: Style(
-                    $box.height(size.height),
-                    $box.width(size.height),
-                  ),
-                  onPressed: () {
-                    _overlayEntry?.remove();
-                    setState(() {
-                      _skin = skin;
-                      widget.onSkinChanged(skin);
-                      _expanded = false;
-                    });
-                  },
-                  child: SkinDot(skin: skin),
-                ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    if (_overlayEntry?.mounted == true) _overlayEntry?.remove();
-    focusNode.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
-    return SkinDotButton(
-      skin: _skin,
-      onPressed: () {
-        if (_expanded) {
-          _overlayEntry?.remove();
-        } else {
-          _overlayEntry = createOverlay(context);
-          Overlay.of(context).insert(_overlayEntry!);
-        }
-        setState(() {
-          _expanded = !_expanded;
-        });
-      },
-    );
+    return LayoutBuilder(builder: (context, constraints) {
+      final size = constraints.biggest;
+
+      return PortalTarget(
+        visible: _expanded,
+        anchor: Aligned(
+          follower: Alignment.bottomRight,
+          target: Alignment.topRight,
+        ),
+        portalFollower: DSToolbar(
+          direction: Axis.horizontal,
+          children: [
+            for (var skin = 0; skin < SkinTones.tones.length; skin++)
+              DSToolbarItem(
+                style: Style(
+                  $box.height(size.height),
+                  $box.width(size.height),
+                ),
+                onPressed: () {
+                  setState(() {
+                    _skin = skin;
+                    widget.onSkinChanged(skin);
+                    _expanded = false;
+                  });
+                },
+                child: SkinDot(skin: skin),
+              ),
+          ],
+        ),
+        child: SkinDotButton(
+          skin: _skin,
+          onPressed: () {
+            setState(() => _expanded = !_expanded);
+          },
+        ),
+      );
+    });
   }
 }
 
